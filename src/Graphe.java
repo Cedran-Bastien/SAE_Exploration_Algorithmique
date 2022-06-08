2d0ce38c0331e118236346f00866b55cafd0ce13
unknown <Cedran-Bastien>
2022-06-08 09:04:47 +0200

    Ajout interface Graphe

---
+++ src/Graphe.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public interface Graphe {
+    /**
+     *
+     * @return
+     *      les noeuds du graphe
+     */
+    public List<String> listeNoeuds();
+
+    /**
+     * rechercge les arc adjacenta un sommet
+     * @param n
+     *      nom du sommet
+     * @return
+     *      liste des arc adjacent
+     */
+    public List<Arc> suivant(String n);
+}
